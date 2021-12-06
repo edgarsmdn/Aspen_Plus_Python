242a9f338bdbab522af5cbb3058de2a0061b624b
Edgar Sanchez <47745870+edgarsmdn@users.noreply.github.com>
2021-12-06 11:19:55 +0100

    files

---
+++ Simulation/connection.ads
@@ -0,0 +1,141 @@
+<AdbFile>
+ <GuidDictionary count="121">
+  <Guid value="e1bdaa8f-05fd-43bb-903e-d87749440aa9" int="0"/>
+  <Guid value="fb058446-e81c-4e84-a1c6-57d0a3202bbe" int="1"/>
+  <Guid value="149049bb-db00-453c-9f0a-ce5f829dfa6a" int="2"/>
+  <Guid value="6139ab55-36b8-4f11-b965-be9af1398548" int="3"/>
+  <Guid value="1240b6a7-26b8-49c3-b71b-c4177c4dfa93" int="4"/>
+  <Guid value="6a0510df-f74e-458c-b813-3a5b26c0cc98" int="5"/>
+  <Guid value="36a46377-00ac-46db-9177-77eb99e36024" int="6"/>
+  <Guid value="7d969648-46e7-40f1-bb48-4e4e8170ed50" int="7"/>
+  <Guid value="5564ad08-a311-48a4-849b-888477a0abdc" int="8"/>
+  <Guid value="e46b55ab-2cdf-4ff3-8a9d-f32c385b7477" int="9"/>
+  <Guid value="b5177582-732b-4ccf-a68d-71bdc76bc010" int="10"/>
+  <Guid value="523c24d6-2679-48b2-90c0-9f4d804e4a50" int="11"/>
+  <Guid value="7aa50a59-73c1-47b9-a27b-72f11886a478" int="12"/>
+  <Guid value="0139c15d-3ec6-44a5-b2d8-19670af93704" int="13"/>
+  <Guid value="d9f77d68-a1a6-41e6-bb0a-4aac78acaeb3" int="14"/>
+  <Guid value="ca70c9ba-15d4-4fbb-afdb-df3f59f1a59b" int="15"/>
+  <Guid value="96c0ded1-6432-40aa-9658-1a8f04b73e5a" int="16"/>
+  <Guid value="cf6dafa7-e227-4be5-b732-bc58327b3ef1" int="17"/>
+  <Guid value="767491b8-0eea-4f62-9772-c4d62d302d89" int="18"/>
+  <Guid value="e1f77723-20af-481b-b715-a247fa28347c" int="19"/>
+  <Guid value="bab39d78-d6e6-4c3e-a0d0-475d93d74206" int="20"/>
+  <Guid value="4e5b2616-eca4-4750-bcae-16144ab4aa9d" int="21"/>
+  <Guid value="0c1b0347-8cdf-4384-9334-b8d0afed0470" int="22"/>
+  <Guid value="b0233e04-f5b3-4cc1-bd10-6242264f3279" int="23"/>
+  <Guid value="3c6b7fbb-4809-4430-a433-c62bf69deb91" int="24"/>
+  <Guid value="fe6faa69-79c7-4d71-a695-300a4171a239" int="25"/>
+  <Guid value="17d557ee-0e3b-4f65-89cf-8988433f57bb" int="26"/>
+  <Guid value="74fd7dac-6707-4cc1-80de-36fadfa7800b" int="27"/>
+  <Guid value="c8ce8a72-9c5e-4fee-8fd2-9b32c6654f87" int="28"/>
+  <Guid value="217df349-385b-40aa-9980-7fa1ec316e4d" int="29"/>
+  <Guid value="0a436056-91cb-47bf-829f-58dcadfb76a4" int="30"/>
+  <Guid value="213b1a73-699e-4a7f-9bcb-7e7ea676b28b" int="31"/>
+  <Guid value="5d203439-3442-4604-a2c1-339025bdb230" int="32"/>
+  <Guid value="34bf561f-ccea-48c9-b039-e0798797098d" int="33"/>
+  <Guid value="2f95107c-cfad-4668-a555-abdf2f0aaa02" int="34"/>
+  <Guid value="d9d5caf1-a5ab-400b-81c4-33dfb2d9633d" int="35"/>
+  <Guid value="53dc327f-6ee6-4caa-abb2-d325670d3951" int="36"/>
+  <Guid value="2f2b54f5-0fe0-47a7-a90a-70da617a58da" int="37"/>
+  <Guid value="3bfc4754-a448-47d1-82bf-a9a246075901" int="38"/>
+  <Guid value="39cf37ab-c038-45c3-b95e-480b422901b7" int="39"/>
+  <Guid value="399248a9-d286-4878-8317-089635e8b44d" int="40"/>
+  <Guid value="8a966293-3e8a-4ce6-a947-995dae50b7bd" int="41"/>
+  <Guid value="e3197534-fa65-46f1-9569-bf68586809be" int="42"/>
+  <Guid value="a3a1b82b-79c9-489d-bb1e-070fcf89444a" int="43"/>
+  <Guid value="47acee2d-56cc-4773-b7e2-ee20225eec53" int="44"/>
+  <Guid value="4e87a2bd-421b-4b63-8ed8-79e387678252" int="45"/>
+  <Guid value="80803248-6b52-4a49-9328-788e4e58db34" int="46"/>
+  <Guid value="b6fec974-2f6f-4339-bfcd-45d1d91b3c26" int="47"/>
+  <Guid value="2348d9d1-decf-4beb-b6cf-571f5f38f08c" int="48"/>
+  <Guid value="2a6bf3bc-c8a1-42b2-b1cb-b8deca24df04" int="49"/>
+  <Guid value="5afcce95-57b9-450c-81ac-9ed152bb5232" int="50"/>
+  <Guid value="e4cbe853-9062-4bb1-ac6f-d226cf5c9897" int="51"/>
+  <Guid value="24a65d64-3fde-4edc-a8cd-09b43e27401d" int="52"/>
+  <Guid value="15593bd7-3af5-4a7f-8731-19f17e296b22" int="53"/>
+  <Guid value="ebe609c5-c41b-4f79-88a6-ed3c0abfb958" int="54"/>
+  <Guid value="ba430646-cca5-47e0-b6f0-7051e3417088" int="55"/>
+  <Guid value="87a9b2c2-c453-42de-8d4d-7d38660bf9bd" int="56"/>
+  <Guid value="1c0f4d8c-db7e-46c7-9d97-d748af00d34b" int="57"/>
+  <Guid value="b56b510f-d77c-473c-8b6a-983df185ef5d" int="58"/>
+  <Guid value="a52e13da-789f-4675-8619-bf83c3085b61" int="59"/>
+  <Guid value="d21adcbc-fdbe-488e-8695-1696bcdf9d96" int="60"/>
+  <Guid value="114dae4a-0e1e-4745-b868-326e39ba8245" int="61"/>
+  <Guid value="3bb34d03-117c-4081-9bb4-bf6781e63d54" int="62"/>
+  <Guid value="ad556ff8-d41d-4030-82f7-97f654cd25aa" int="63"/>
+  <Guid value="73fa2a41-c074-46bb-87f2-31d32bd8a231" int="64"/>
+  <Guid value="c690f9dd-0a30-4ae6-8055-34c0fc58c300" int="65"/>
+  <Guid value="f127c0f6-d60f-45f8-9a6e-28a51482b944" int="66"/>
+  <Guid value="37e01851-1620-4fb6-885f-f4a401bb2a63" int="67"/>
+  <Guid value="36adff82-c12d-4b22-9ac5-1e4bc786ed91" int="68"/>
+  <Guid value="aac6dd64-f01f-4ebf-b4c8-57c4b850a9be" int="69"/>
+  <Guid value="4ed0cdbb-bb83-4e2d-a59d-5adf243aef69" int="70"/>
+  <Guid value="d4198184-cf6e-41e8-b963-e3b5b1352ebb" int="71"/>
+  <Guid value="78ff734a-b78e-45c4-8616-2f82e26db057" int="72"/>
+  <Guid value="613a616b-e114-476a-a337-ca2f3db02fa0" int="73"/>
+  <Guid value="038976a1-9c97-4d62-b034-5cddf6d0c3d9" int="74"/>
+  <Guid value="9e1eb42e-4da8-4e40-8ccc-22e7a49ab434" int="75"/>
+  <Guid value="d3d425b1-27cc-48e5-b0c1-c17bd6c4ec08" int="76"/>
+  <Guid value="5729a872-3fa6-47ad-b309-341a16bcaa62" int="77"/>
+  <Guid value="038b16f2-e3ac-4dbb-92eb-ca9000184a22" int="78"/>
+  <Guid value="f47d9357-2c29-4fff-ad60-e9a0e39a2fe2" int="79"/>
+  <Guid value="c3ecab21-3cac-45c0-9709-8ad2313b8457" int="80"/>
+  <Guid value="190e49ac-da04-4937-a031-36d72d2747d2" int="81"/>
+  <Guid value="a5ba7c30-38d8-4568-b222-7b634e30f119" int="82"/>
+  <Guid value="d00bf64a-073b-4032-a75c-87b9edc41bab" int="83"/>
+  <Guid value="8907541b-63a8-4307-97b9-fc23bb4f7ce3" int="84"/>
+  <Guid value="9b616bc1-a38c-42ba-84f0-d41aa3fe3c0d" int="85"/>
+  <Guid value="7589f4a9-7d03-4306-98fd-48c927006d97" int="86"/>
+  <Guid value="b2befa81-d55f-470b-9549-0a93d5a8dfba" int="87"/>
+  <Guid value="8151f41d-739f-4a81-b8a7-90b9df5df93a" int="88"/>
+  <Guid value="f92f607c-0874-4f5b-90ee-f165edb8e2ab" int="89"/>
+  <Guid value="643f1978-21c9-4529-a95c-3f8e52072309" int="90"/>
+  <Guid value="3fc900c7-d0f0-4f28-9799-a033461dbab2" int="91"/>
+  <Guid value="b65ad35e-bc15-4f1e-ba4a-0cee40c516bd" int="92"/>
+  <Guid value="a5ff5f3a-f605-4bce-b9aa-20c7585b200d" int="93"/>
+  <Guid value="cbe6d848-93e7-4490-9ed4-6e52fbc1b049" int="94"/>
+  <Guid value="04384086-a1b7-4d1a-a185-1afa035459a3" int="95"/>
+  <Guid value="7e7d5fd8-b283-4b8f-98af-113fcd1d8e71" int="96"/>
+  <Guid value="2194afbf-4c01-424a-a598-2d6b41502805" int="97"/>
+  <Guid value="9f67acf5-ecce-4045-84f0-467fa3de01be" int="98"/>
+  <Guid value="565a9ba8-597c-4394-9361-89564924fe3e" int="99"/>
+  <Guid value="9f0bd91f-1b59-4473-9f30-cccf63f6787f" int="100"/>
+  <Guid value="c464ad1e-98d4-4f1d-b563-4f9832104b7c" int="101"/>
+  <Guid value="0e9262d9-2ea9-45ce-b21c-bfd4aba42cf0" int="102"/>
+  <Guid value="8e4f9fb5-1f4a-4a9c-bc51-e561eddf40b7" int="103"/>
+  <Guid value="28595021-483a-460d-bef7-03ab147c6414" int="104"/>
+  <Guid value="d115f21f-e29c-405c-83f1-d05fd3dfc13e" int="105"/>
+  <Guid value="c43ac5fa-0d0d-4795-84ae-47e122458aeb" int="106"/>
+  <Guid value="d0835727-9d32-4cd3-9e69-21bff34aea49" int="107"/>
+  <Guid value="8b9185ba-7f5e-47fe-83b2-6d74ce612691" int="108"/>
+  <Guid value="86c17b98-cc01-4041-8d19-f70ebc0ba843" int="109"/>
+  <Guid value="e773b742-2e93-4702-ad45-928f7a84c155" int="110"/>
+  <Guid value="9434c195-c627-46b5-bbf4-d47a8a733ce4" int="111"/>
+  <Guid value="1147f2c6-4c68-475d-8904-d6ff18e0e150" int="112"/>
+  <Guid value="206faf60-f74c-484d-b762-c906f4a44b7f" int="113"/>
+  <Guid value="4013184f-56a0-4814-b528-32bd7e6b378a" int="114"/>
+  <Guid value="ced8fabc-e7e0-4826-b6a4-22a3b27f091a" int="115"/>
+  <Guid value="9f0c959e-1756-432b-b90b-f254e111098c" int="116"/>
+  <Guid value="2465d752-efdc-412a-a8ef-ec4ec7c29326" int="117"/>
+  <Guid value="572c7c2f-69d1-4014-8168-39c7745f8b7e" int="118"/>
+  <Guid value="a1025aec-4590-4163-891f-d8c433a74643" int="119"/>
+  <Guid value="3aa58576-347d-47ce-bcdf-660a94fa8efc" int="120"/>
+ </GuidDictionary>
+ <Records>
+  <Record typeGuid="1" guid="0"><Value guid="2">21</Value></Record>
+  <Record typeGuid="22" guid="21"><Array guid="26"><Element><Key>COL-EXT</Key><Val>30</Val></Element><Element><Key>MXSOLV</Key><Val>116</Val></Element></Array><Array guid="27"><Element><Key>FEED</Key><Val>53</Val></Element><Element><Key>C7</Key><Val>110</Val></Element><Element><Key>RICH-SOL</Key><Val>112</Val></Element><Element><Key>SOLVENT</Key><Val>114</Val></Element><Element><Key>MAKE-UP</Key><Val>118</Val></Element></Array></Record>
+  <Record typeGuid="31" guid="30"><Value guid="32">false</Value><Value guid="39">true</Value><Value guid="40">true</Value><Value guid="41">false</Value><Value guid="42">false</Value><Value guid="43">false</Value><Value guid="45">COL-EXT</Value><Value guid="47">21</Value><Array guid="50"><Element><Val>105</Val></Element><Element><Val>115</Val></Element></Array><Array guid="51"><Element><Val>111</Val></Element><Element><Val>113</Val></Element></Array></Record>
+  <Record typeGuid="54" guid="53"><Value guid="55">FEED</Value><Value guid="56">1</Value><Value guid="57">21</Value><Value guid="59">105</Value><Value guid="95">0</Value></Record>
+  <Record typeGuid="106" guid="105"><Value guid="107">F(IN)</Value><Value guid="108">30</Value><Value guid="109">53</Value></Record>
+  <Record typeGuid="54" guid="110"><Value guid="55">C7</Value><Value guid="56">1</Value><Value guid="57">21</Value><Value guid="58">111</Value><Value guid="95">0</Value></Record>
+  <Record typeGuid="106" guid="111"><Value guid="107">VD(OUT)</Value><Value guid="108">30</Value><Value guid="109">110</Value></Record>
+  <Record typeGuid="54" guid="112"><Value guid="55">RICH-SOL</Value><Value guid="56">1</Value><Value guid="57">21</Value><Value guid="58">113</Value><Value guid="95">0</Value></Record>
+  <Record typeGuid="106" guid="113"><Value guid="107">B(OUT)</Value><Value guid="108">30</Value><Value guid="109">112</Value></Record>
+  <Record typeGuid="54" guid="114"><Value guid="55">SOLVENT</Value><Value guid="56">1</Value><Value guid="57">21</Value><Value guid="58">120</Value><Value guid="59">115</Value><Value guid="95">0</Value></Record>
+  <Record typeGuid="106" guid="115"><Value guid="107">F(IN)</Value><Value guid="108">30</Value><Value guid="109">114</Value></Record>
+  <Record typeGuid="117" guid="116"><Value guid="39">true</Value><Value guid="40">true</Value><Value guid="41">false</Value><Value guid="42">false</Value><Value guid="43">false</Value><Value guid="45">MXSOLV</Value><Value guid="47">21</Value><Array guid="50"><Element><Val>119</Val></Element></Array><Array guid="51"><Element><Val>120</Val></Element></Array></Record>
+  <Record typeGuid="54" guid="118"><Value guid="55">MAKE-UP</Value><Value guid="56">1</Value><Value guid="57">21</Value><Value guid="59">119</Value><Value guid="95">0</Value></Record>
+  <Record typeGuid="106" guid="119"><Value guid="107">F(IN)</Value><Value guid="108">116</Value><Value guid="109">118</Value></Record>
+  <Record typeGuid="106" guid="120"><Value guid="107">P(OUT)</Value><Value guid="108">116</Value><Value guid="109">114</Value></Record>
+ </Records></AdbFile>
